51336281f24bd4b05565258244b2f3f5b8c18c16
rafasdiass <128534833+rafasdiass@users.noreply.github.com>
2023-07-05 10:41:08 -0300

    status

--- src/app/components/loan-status/loan-status.component.ts
+++ src/app/components/loan-status/loan-status.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-loan-status',
   templateUrl: './loan-status.component.html',
   styleUrls: ['./loan-status.component.scss']
 })
-export class LoanStatusComponent {
+export class LoanStatusComponent implements OnInit {
+  loanStatus: string = '';
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get('http://localhost:3000/loan-status')
+      .pipe(map((response: any) => {
+        this.loanStatus = response.status;
+      }))
+      .subscribe();
+  }
 }
