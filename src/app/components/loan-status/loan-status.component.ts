4908ff300328b99c16649b8ce20610922f2d3994
rafasdiass <128534833+rafasdiass@users.noreply.github.com>
2023-07-06 10:06:29 -0300

    forms

--- src/app/components/loan-status/loan-status.component.ts
+++ src/app/components/loan-status/loan-status.component.ts
@@ -1,37 +1,28 @@
 // loan-status.component.ts
 
-import { Component, AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { LoanService } from '../../services/loan.service';
 import { LoanStatusResponse } from '../../models/loanstatusresponse.model';
-
-declare var bootstrap: any;
 
 @Component({
   selector: 'app-loan-status',
   templateUrl: './loan-status.component.html',
   styleUrls: ['./loan-status.component.scss']
 })
-export class LoanStatusComponent implements AfterViewInit {
+export class LoanStatusComponent {
   name: string = '';
   documentNumber: string = '';
 
   loanStatusResponse: LoanStatusResponse | null = null;
 
-  private modal: any;
+  hideCPF: boolean = false; // Nova propriedade para controlar a exibição do CPF/CNPJ
 
   constructor(private loanService: LoanService) {}
-
-  ngAfterViewInit() {
-    this.modal = new bootstrap.Modal(document.getElementById('statusModal'), {
-      keyboard: false
-    });
-  }
 
   onCheckStatus() {
     this.loanService.getLoanStatus(this.name, this.documentNumber)
       .subscribe((response: LoanStatusResponse) => {
         this.loanStatusResponse = response;
-        this.modal.show();
       }, (error) => {
         console.error('Error:', error);
         this.loanStatusResponse = null;
