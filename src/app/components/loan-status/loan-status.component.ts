c0da00930deea7881cfdbd1bd7c77d0653ae46a5
rafasdiass <128534833+rafasdiass@users.noreply.github.com>
2023-07-05 23:21:41 -0300

    modal

--- src/app/components/loan-status/loan-status.component.ts
+++ src/app/components/loan-status/loan-status.component.ts
@@ -1,26 +1,37 @@
 // loan-status.component.ts
 
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { LoanService } from '../../services/loan.service';
 import { LoanStatusResponse } from '../../models/loanstatusresponse.model';
+
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-loan-status',
   templateUrl: './loan-status.component.html',
   styleUrls: ['./loan-status.component.scss']
 })
-export class LoanStatusComponent {
+export class LoanStatusComponent implements AfterViewInit {
   name: string = '';
   documentNumber: string = '';
 
   loanStatusResponse: LoanStatusResponse | null = null;
 
-  constructor(private loanService: LoanService) { }
+  private modal: any;
+
+  constructor(private loanService: LoanService) {}
+
+  ngAfterViewInit() {
+    this.modal = new bootstrap.Modal(document.getElementById('statusModal'), {
+      keyboard: false
+    });
+  }
 
   onCheckStatus() {
     this.loanService.getLoanStatus(this.name, this.documentNumber)
       .subscribe((response: LoanStatusResponse) => {
         this.loanStatusResponse = response;
+        this.modal.show();
       }, (error) => {
         console.error('Error:', error);
         this.loanStatusResponse = null;
