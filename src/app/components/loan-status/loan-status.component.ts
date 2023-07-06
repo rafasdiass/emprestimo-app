dba91517c48985604d88258505db14a95acdbc3b
rafasdiass <128534833+rafasdiass@users.noreply.github.com>
2023-07-06 14:41:20 -0300

    finalizando

--- src/app/components/loan-status/loan-status.component.ts
+++ src/app/components/loan-status/loan-status.component.ts
@@ -1,8 +1,8 @@
 // loan-status.component.ts
-
 import { Component } from '@angular/core';
 import { LoanService } from '../../services/loan.service';
 import { LoanStatusResponse } from '../../models/loanstatusresponse.model';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-loan-status',
@@ -12,17 +12,16 @@ import { LoanStatusResponse } from '../../models/loanstatusresponse.model';
 export class LoanStatusComponent {
   name: string = '';
   documentNumber: string = '';
-
   loanStatusResponse: LoanStatusResponse | null = null;
-
   hideCPF: boolean = false; // Nova propriedade para controlar a exibição do CPF/CNPJ
 
-  constructor(private loanService: LoanService) {}
+  constructor(private loanService: LoanService, private modalService: NgbModal) {}
 
-  onCheckStatus() {
+  onCheckStatus(content: any) {
     this.loanService.getLoanStatus(this.name, this.documentNumber)
       .subscribe((response: LoanStatusResponse) => {
         this.loanStatusResponse = response;
+        this.modalService.open(content); // open the modal
       }, (error) => {
         console.error('Error:', error);
         this.loanStatusResponse = null;
